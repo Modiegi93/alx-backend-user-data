ba1b8e22e356eefb6a29f1ee08314739f19cb163
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-08-03 08:57:34 +0200

    initial commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Usage of regex to replace occurrences of certain values"""
 import re
 import logging
+import csv
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -18,7 +19,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields):
         """Initializing the formatter class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
@@ -28,3 +29,20 @@ class RedactingFormatter(logging.Formatter):
         for field in self.fields:
             record.msg = re.sub(rf'{field}=.*?;', rf'{field}={self.REDACTION};', record.msg)
         return super(RedactingFormatter, self).format(record) 
+
+
+def get_logger() -> logging.Logger:
+    """Get logger details"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
+PII_FIELDS = ("name", "email", "phone", "credit_card", "address")
