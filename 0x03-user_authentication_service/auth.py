50bbd4dacfa4b23ddf5f31666bf3a129bf180db3
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-08-16 11:08:23 +0200

    update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,8 +82,8 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
-            reset_token = _generate_uuid()
+        reset_token = _generate_uuid()
 
-            self._db.update_user(user.id, reset_token=reset_token)
+        self._db.update_user(user.id, reset_token=reset_token)
 
-            return reset_token
+        return reset_token
